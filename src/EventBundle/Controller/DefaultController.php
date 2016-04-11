3709a6c1da5d79148ebf11c3a34f794be07f9f8a
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-04-11 19:42:37 +0100

    Change response for a templating view

--- src/EventBundle/Controller/DefaultController.php
+++ src/EventBundle/Controller/DefaultController.php
@@ -3,18 +3,17 @@
 namespace EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($count, $name)
     {
-        $data = [
-            'count' => $count,
-            'name' => $name,
-            'ackbar' => "It's a traaaaaap!"
-        ];
+        $templating = $this->container->get('templating');
 
-        return new JsonResponse($data);
+        $content = $templating->render('EventBundle:Default:index.html.twig',
+            ['name' => $name]);
+
+        return new Response($content);
     }
 }
